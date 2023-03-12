3a8c2fa0ce5680fc0366b42f9d1e8346f0b8c6d6
egorrrmiller <egorrr.miller@yandex.ru>
2023-03-12 17:13:20 +0300

    Почистил контекст

--- DataBase/Context/KaspelContext.cs
+++ DataBase/Context/KaspelContext.cs
@@ -19,89 +19,12 @@ public class KaspelContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        // долго сидел с попыткой сделать связь один-ко-многим, но ничего не получалось, хотя делал вроде как правильно...
-        // по итогу сделал связь многие-ко-многим
-
         modelBuilder.Entity<Order>(o =>
         {
             o
                 .HasMany(a => a.Books)
                 .WithMany(b => b.Orders)
-                .UsingEntity<OrderBook>(
-                    book => book
-                        .HasOne(b => b.Book)
-                        .WithMany(o => o.OrderBooks)
-                        .HasForeignKey(id => id.BookId),
-                    order => order
-                        .HasOne(o => o.Order)
-                        .WithMany(b => b.OrderBooks)
-                        .HasForeignKey(id => id.OrderId));
+                .UsingEntity<OrderBook>();
         });
-
-        /*modelBuilder.Entity<Book>(book =>
-        {
-            book.HasData(new Book()
-            {
-                Id = 1,
-                Title = "titele 1",
-                ReleaseDate = DateTime.Now
-            }, new Book()
-            {
-                Id = 2,
-                Title = "titele 2",
-                ReleaseDate = DateTime.Now
-            }, new Book()
-            {
-                Id = 3,
-                Title = "titele 3",
-                ReleaseDate = DateTime.Now
-            });
-        });
-
-        modelBuilder.Entity<Order>(o =>
-        {
-            o.HasData(new Order()
-            {
-                Id = 1,
-                OrderDate = DateTime.Now
-            },new Order()
-            {
-                Id = 2,
-                OrderDate = DateTime.Now
-            },new Order()
-            {
-                Id = 3,
-                OrderDate = DateTime.Now
-            });
-        });
-
-        modelBuilder.Entity<OrderBook>(b =>
-        {
-            b.HasData(new OrderBook()
-            {
-                BookId = 1,
-                OrderId = 1
-            }, new OrderBook()
-            {
-                BookId = 2,
-                OrderId = 1
-            }, new OrderBook()
-            {
-                BookId = 3,
-                OrderId = 1
-            }, new OrderBook()
-            {
-                BookId = 1,
-                OrderId = 2
-            }, new OrderBook()
-            {
-                BookId = 2,
-                OrderId = 2
-            }, new OrderBook()
-            {
-                BookId = 3,
-                OrderId = 2
-            });
-        });*/
     }
 }
